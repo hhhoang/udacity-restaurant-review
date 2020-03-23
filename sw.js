2ff7bab261792505bf2bf0d2b622f27e041e75df
hhhoang <thimyhanh.hoang@gmail.com>
2020-03-23 20:42:35 +0100

    add some code comments

--- sw.js
+++ sw.js
@@ -1,6 +1,8 @@
 // code mainly using from wittr project from Udaicty
 let staticCacheName = 'restaurant-reviews-v1';
 
+
+// service worker install event
 self.addEventListener('install', function(event) {
     event.waitUntil(caches.open(staticCacheName)
         .then(function(cache) {
@@ -27,6 +29,7 @@ self.addEventListener('install', function(event) {
         }));
 });
 
+// service worker activate event
 self.addEventListener('activate', function(event) {
     event.waitUntil(caches.keys()
         .then(function(cacheNames) {
@@ -39,6 +42,7 @@ self.addEventListener('activate', function(event) {
         }));
 });
 
+// service worker fetch event
 self.addEventListener('fetch', function(event) {
     console.log(event);
     event.respondWith(caches.match(event.request).then(function(response) {
